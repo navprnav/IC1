01da1bc421f8fe9caa28c13b0bb2dad770336cfd
navprnav <navprnav@sheridancollege.ca>
2022-01-27 14:33:49 -0500

    ICE1

---
+++ src/cardtrickice1/Card.java
@@ -0,0 +1,43 @@
+
+package cardtrickice1;
+
+public class Card {
+  private int value;
+          private String suits; 
+          String[]Suits={"diamomnd","clubs","spades","hearts"};
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }                   //https://github.com/sivagamasheridan/ICE1_Wednesday_22.git
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    /**
+     * @return the suits
+     */
+    public String getSuits() {
+        return suits;
+    }
+
+    /**
+     * @param suits the suits to set
+     */
+    public void setSuits(String suits) {
+        this.suits = suits;
+    }
+
+    /**
+     * @param Suits the Suits to set
+     */
+    public void setSuits(String[] Suits) {
+        this.Suits = Suits;
+    }
+}
---
+++ src/cardtrickice1/CardtrickICE1.java
@@ -0,0 +1,22 @@
+
+package cardtrickice1;
+
+
+public class CardtrickICE1 {
+
+    
+    public static void main(String[] args) 
+    {
+        Card[] magicHand=new Card[7];
+        for(int i=0;i<magicHand.length;i++)
+        {
+        Card c1=new Card();
+        c1.setValue(2); //use a methode to generate random method*13
+        c1.setSuits("hearts"); //random method
+        }
+        //step 2:take input
+        
+        //step 3:match with array
+    }
+    
+}
