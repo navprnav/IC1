9b12aa83c2c850db0d26b8f70b47ba0f94a1b9af
navprnav <navprnav@sheridancollege.ca>
2022-02-01 15:46:36 -0500

    Branch Code

--- src/cardtrickice1/CardtrickICE1.java
+++ src/cardtrickice1/CardtrickICE1.java
@@ -13,36 +13,32 @@ public class CardtrickICE1 {
     
     public static void main(String[] args) 
     {
-        //variables and objects
+        
         Scanner scan = new Scanner(System.in);
-        Random ranCard=new Random();
-        Card[] magicHand = new Card[7]; //Array of object
-        
-        //display the hand
-        System.out.println("The cards in the magic hand are:");
-        
-        //for statement to generate cards
+        Random randomCard=new Random();
+        Card[] magicHand = new Card[7];
+       
         for(int i=0;i<magicHand.length;i++) 
         {
         Card c1=new Card();
-        c1.setValue(ranCard.nextInt(13)+1); //use a methode to generate random method*13
-        c1.setSuits(Card.SUITS[ranCard.nextInt(3)]); //random method
+        c1.setValue(randomCard.nextInt(13)+1); 
+        c1.setSuits(Card.SUITS[randomCard.nextInt(3)]); 
         magicHand[i]=c1;
         System.out.println(c1);       
         }
         
-        //asking for the user a card
-        System.out.print("Enter card value: ");
-        int cardvalue= scan.nextInt();
-        System.out.print("Enter suit: ");
-        String suit = scan.next();
         
-        //creating a new object
+        System.out.print("Enter the value of Card: ");
+        int valueofCard= scan.nextInt();
+        System.out.print("Enter the Suit: ");
+        String thesuit = scan.next();
+        
+        
         Card cardTrick= new Card();
-        cardTrick.setValue(cardvalue);
-        cardTrick.setSuits(suit);
+        cardTrick.setValue(valueofCard);
+        cardTrick.setSuits(thesuit);
         
-        //for statement to compare the objects
+       
         boolean found = false;
         System.out.println("Searching for: " + cardTrick);
         
